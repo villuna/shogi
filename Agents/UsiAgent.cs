d0b4a0dbd76b6fcaf2f24a97ae1cc7a58f813bdc
Luna Borella <ulfrthewolf@gmail.com>
2025-10-29 01:23:28 +1000

    Launching and communicating with the shogi bot

--- Agents/UsiAgent.cs
+++ Agents/UsiAgent.cs
@@ -3,4 +3,25 @@ using System;
 
 public partial class UsiAgent : Node
 {
+    [Export]
+    public String processPath;
+
+    private FileAccess processStdio;
+
+    public override void _Ready()
+    {
+        Godot.Collections.Dictionary dict =
+            OS.ExecuteWithPipe(processPath, [""], blocking: false);
+        processStdio = (FileAccess)dict["stdio"];
+        processStdio.StoreLine("usi");
+    }
+
+    public override void _Process(double delta)
+    {
+        String nextLine = processStdio.GetLine();
+        if (nextLine.Length != 0)
+        {
+            GD.Print("nano: \"" + nextLine + "\"");
+        }
+    }
 }
